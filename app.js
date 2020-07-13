0a7e6818e469cccf4849508d0e90fe3619ae58ae
Santi Bello <santidebello@gmail.com>
2020-07-13 14:29:13 +0200

    Better spam filter

--- app.js
+++ app.js
@@ -16,13 +16,17 @@ var T = new Twit({
 var http = require('http');
 var server = http.createServer(function(req, res) {
   res.writeHead(200, {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'});
-  T.get('search/tweets', { q: '#AnimalCrossing turnip', result_type: "recent", count: 100,}, 
+  T.get('search/tweets', { q: '#AnimalCrossing turnip', result_type: "recent", count: 120,}, 
   (err, data, response) => {
     let tweets = data.statuses
       .filter(tweet => !tweet.retweeted_status) // No RTs
       .filter(tweet => !tweet.text.includes("@"))
+      .filter(tweet => !tweet.text.toLowerCase().includes("rt"))
+      .filter(tweet => !tweet.text.toLowerCase().includes("like"))
+      .filter(tweet => !tweet.text.toLowerCase().includes("reply"))
       .map((tweet, i, a ) => {
         return { // Gets tweet's attributes we want
+          alltweet : tweet, 
           id   : tweet.id_str,
           screen_name : tweet.user.screen_name,
           text : tweet.text,
