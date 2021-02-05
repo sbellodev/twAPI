e810eae55872bca3a6dab63a27c3a33b59028849
Santi Bello <santidebello@gmail.com>
2021-02-05 22:14:42 +0100

    update to full text

--- app.js
+++ app.js
@@ -16,26 +16,26 @@ var T = new Twit({
 var http = require('http');
 var server = http.createServer(function(req, res) {
   res.writeHead(200, {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json', 'Cache-Control' : 'max-age=60'});
-  T.get('search/tweets', { q: '#AnimalCrossing turnip', result_type: "recent", count: 120,}, 
+  T.get('search/tweets', { tweet_mode: 'extended', q: '#AnimalCrossing turnip', result_type: "recent", count: 200,}, 
   (err, data, response) => {
     let tweets = data.statuses
       .filter(tweet => !tweet.retweeted_status) // No RTs
-      .filter(tweet => !tweet.text.includes("@"))
-      .filter(tweet => !tweet.text.toLowerCase().includes("rt"))
-      .filter(tweet => !tweet.text.toLowerCase().includes("like"))
-      .filter(tweet => !tweet.text.toLowerCase().includes("reply"))
+      .filter(tweet => !tweet.full_text.includes("@"))
+      .filter(tweet => !tweet.full_text.toLowerCase().includes("rt"))
+      .filter(tweet => !tweet.full_text.toLowerCase().includes("like"))
+      .filter(tweet => !tweet.full_text.toLowerCase().includes("reply"))
       .map((tweet, i, a ) => {
         return { // Gets tweet's attributes we want
           id   : tweet.id_str,
           screen_name : tweet.user.screen_name,
           profile_image : tweet.user.profile_image_url,
-          text : tweet.text,
+          text : tweet.full_text,
           entities : tweet.entities
         }
       })
-    if(tweets.length > 10) {tweets.length = 10}
+    if(tweets.length > 25) {tweets.length = 25}
     tweets.map(tw => console.log(tw))
     res.end(JSON.stringify(tweets));
   })
 });
-server.listen(9999);
+server.listen();
