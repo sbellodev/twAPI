88b4962c10682f998497e3fe8c5c0dd213d80234
Santi Bello <santidebello@gmail.com>
2021-02-05 18:35:40 +0100

    Revert "add full text tweets"
    This reverts commit 2b00a02d080d5bc3bd24b9e2968c11e3c2c9e7cb.

--- app.js
+++ app.js
@@ -14,17 +14,18 @@ var T = new Twit({
 });
 
 var http = require('http');
-const delTweet = ['@', 'rt', 'like', 'reply']
-
 var server = http.createServer(function(req, res) {
   res.writeHead(200, {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json', 'Cache-Control' : 'max-age=60'});
-  T.get('search/tweets', {tweet_mode: 'extended', q: '#AnimalCrossing turnip', result_type: 'recent', count: 120}, 
+  T.get('search/tweets', { q: '#AnimalCrossing turnip', result_type: "recent", count: 120,}, 
   (err, data, response) => {
     let tweets = data.statuses
-      .filter(tweet => !tweet.retweeted_status) 
-      .filter(tweet => !delTweet.includes(tweet.full_text))
+      .filter(tweet => !tweet.retweeted_status) // No RTs
+      .filter(tweet => !tweet.text.includes("@"))
+      .filter(tweet => !tweet.text.toLowerCase().includes("rt"))
+      .filter(tweet => !tweet.text.toLowerCase().includes("like"))
+      .filter(tweet => !tweet.text.toLowerCase().includes("reply"))
       .map((tweet, i, a ) => {
-        return { 
+        return { // Gets tweet's attributes we want
           id   : tweet.id_str,
           screen_name : tweet.user.screen_name,
           profile_image : tweet.user.profile_image_url,
@@ -32,7 +33,7 @@ var server = http.createServer(function(req, res) {
           entities : tweet.entities
         }
       })
-    if(tweets.length > 25) {tweets.length = 25}
+    if(tweets.length > 10) {tweets.length = 10}
     tweets.map(tw => console.log(tw))
     res.end(JSON.stringify(tweets));
   })
